88ea5066d0ae566627dc2433b3b5c469518698c0
sekaiacg <sekaiacg@gmail.com>
2023-01-25 18:40:19 +0800

    cmake: Refactor
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- build/cmake/erofs-tools/mkfs.cmake
+++
@@ -1,9 +0,0 @@
-set(TARGET mkfs.erofs)
-
-file(GLOB MKFS_SRCS "${PROJECT_SOURCE_DIR}/mkfs/*.c")
-
-add_executable(${TARGET} ${MKFS_SRCS})
-
-target_compile_options(${TARGET} PRIVATE ${EROFS_UTILS_DEFAULTS_CFLAGS})
-
-target_link_libraries(${TARGET} erofs)
---
+++ build/cmake/lib/libbase.cmake
@@ -0,0 +1,51 @@
+set(TARGET base)
+
+set(TARGET_SRC_DIR "${LIB_DIR}/libbase")
+
+set(TARGET_CFLAGS
+	"-Wall"
+	"-Werror"
+	"-Wextra"
+	"-D_FILE_OFFSET_BITS=64"
+	"-Wexit-time-destructors"
+	"-Wno-c99-designator"
+)
+
+set(libbase_srcs
+	"${TARGET_SRC_DIR}/abi_compatibility.cpp"
+	"${TARGET_SRC_DIR}/chrono_utils.cpp"
+	"${TARGET_SRC_DIR}/cmsg.cpp"
+	"${TARGET_SRC_DIR}/file.cpp"
+	"${TARGET_SRC_DIR}/hex.cpp"
+	"${TARGET_SRC_DIR}/logging.cpp"
+	"${TARGET_SRC_DIR}/mapped_file.cpp"
+	"${TARGET_SRC_DIR}/parsebool.cpp"
+	"${TARGET_SRC_DIR}/parsenetaddress.cpp"
+	"${TARGET_SRC_DIR}/posix_strerror_r.cpp"
+	"${TARGET_SRC_DIR}/process.cpp"
+	"${TARGET_SRC_DIR}/properties.cpp"
+	"${TARGET_SRC_DIR}/stringprintf.cpp"
+	"${TARGET_SRC_DIR}/strings.cpp"
+	"${TARGET_SRC_DIR}/threads.cpp"
+	"${TARGET_SRC_DIR}/test_utils.cpp"
+)
+
+if (CMAKE_SYSTEM_NAME MATCHES "Linux|Darwin")
+	list(APPEND libbase_srcs "${TARGET_SRC_DIR}/errors_unix.cpp" )
+elseif (CMAKE_SYSTEM_NAME MATCHES "Android")
+	list(APPEND TARGET_CFLAGS
+        	"-D_FILE_OFFSET_BITS=64"
+	)
+endif()
+
+add_library(${TARGET} STATIC ${libbase_srcs})
+
+target_include_directories(${TARGET} PRIVATE
+	${libbase_headers}
+	${liblog_headers}
+)
+
+target_compile_options(${TARGET} PRIVATE
+	"$<$<COMPILE_LANGUAGE:C>:${TARGET_CFLAGS}>"
+	"$<$<COMPILE_LANGUAGE:CXX>:${TARGET_CFLAGS}>"
+)
