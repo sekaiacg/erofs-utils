a84fe4158b9096a379f350fb7230602e78c1f906
sekaiacg <sekaiacg@gmail.com>
2023-01-25 18:40:19 +0800

    cmake: Refactor
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- build/cmake/erofs-tools/fsck.cmake
+++
@@ -1,9 +0,0 @@
-set(TARGET fsck.erofs)
-
-file(GLOB FSCK_SRCS "${PROJECT_SOURCE_DIR}/fsck/*.c")
-
-add_executable(${TARGET} ${FSCK_SRCS})
-
-target_compile_options(${TARGET} PRIVATE ${EROFS_UTILS_DEFAULTS_CFLAGS})
-
-target_link_libraries(${TARGET} erofs)
