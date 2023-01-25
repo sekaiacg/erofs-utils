cfa19ff10cd9730d28cf368cb7b1e8a39ba0d9ff
sekaiacg <sekaiacg@gmail.com>
2023-01-25 18:40:19 +0800

    cmake: Refactor
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

---
+++ build/cmake/erofs-tools/erofs_tools.cmake
@@ -0,0 +1,75 @@
+set(common_compile_flags
+	"$<$<COMPILE_LANGUAGE:C>:${LIBEROFS_STATIC_DEFAULTS_CFLAGS}>"
+	"$<$<COMPILE_LANGUAGE:CXX>:${LIBEROFS_STATIC_DEFAULTS_CFLAGS}>"
+)
+
+set(common_headers
+	${liberofs_headers}
+	${libcutils_headers}
+	${libselinux_headers}
+	${libe2fsprogs_headers}
+)
+
+set(common_static_link_lib
+	-Wl,--start-group
+	base
+	cutils
+	log
+	selinux
+	pcre2
+	ext2_uuid
+	lz4_static
+	liblzma
+	fuse_static
+	dl
+	erofs_static
+	-Wl,--end-group
+)
+
+if (CMAKE_SYSTEM_NAME MATCHES "Android")
+	list(APPEND common_static_lib packagelistparser)
+endif()
+
+###############################------mkfs.erofs------###############################
+set(TARGET_mkfs mkfs.erofs)
+file(GLOB mkfs_srcs "${PROJECT_ROOT_DIR}/mkfs/*.c")
+add_executable(${TARGET_mkfs} ${mkfs_srcs})
+target_include_directories(${TARGET_mkfs} PRIVATE ${common_headers})
+target_link_libraries(${TARGET_mkfs} ${common_static_link_lib})
+target_compile_options(${TARGET_mkfs} PRIVATE ${common_compile_flags})
+##################################################################################
+
+###############################------dump.erofs------###############################
+set(TARGET_dump dump.erofs)
+file(GLOB dump_srcs "${PROJECT_ROOT_DIR}/dump/*.c")
+add_executable(${TARGET_dump} ${dump_srcs})
+target_include_directories(${TARGET_dump} PRIVATE ${common_headers})
+target_link_libraries(${TARGET_dump} ${common_static_link_lib})
+target_compile_options(${TARGET_dump} PRIVATE ${common_compile_flags})
+
+###############################------fsck.erofs------###############################
+set(TARGET_fsck fsck.erofs)
+file(GLOB fsck_srcs "${PROJECT_ROOT_DIR}/fsck/*.c")
+add_executable(${TARGET_fsck} ${fsck_srcs})
+target_include_directories(${TARGET_fsck} PRIVATE ${common_headers})
+target_link_libraries(${TARGET_fsck} ${common_static_link_lib})
+target_compile_options(${TARGET_fsck} PRIVATE ${common_compile_flags})
+##################################################################################
+
+###############################------fuse.erofs------###############################
+set(TARGET_fuse fuse.erofs)
+file(GLOB fuse_srcs "${PROJECT_ROOT_DIR}/fuse/*.c")
+
+add_executable(${TARGET_fuse} ${fuse_srcs})
+target_precompile_headers(${TARGET_fuse} PRIVATE "${PROJECT_ROOT_DIR}/fuse/macosx.h")
+target_include_directories(${TARGET_fuse} PRIVATE ${common_headers} ${libfuse_headers})
+target_link_libraries(${TARGET_fuse} ${common_static_link_lib})
+if (CMAKE_SYSTEM_NAME MATCHES "Linux|Darwin")
+	target_link_options(${TARGET_fuse} PRIVATE "-Wl,--no-fatal-warnings")
+endif()
+target_compile_options(${TARGET_fuse} PRIVATE
+	${common_compile_flags}
+	"$<$<COMPILE_LANGUAGE:C>:${LIBFUSE_DEFAULTS_CFLAGS}>"
+	"$<$<COMPILE_LANGUAGE:CXX>:${LIBFUSE_DEFAULTS_CFLAGS}>"
+)
+##################################################################################
