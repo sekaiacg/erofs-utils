b0d0accf69462a3d9a948d25f8bb2960fc94f34e
sekaiacg <sekaiacg@gmail.com>
2023-02-24 13:09:24 +0800

    extract: Add extract.erofs
    usage: [options]
      -h, --help          Display this help and exit
      -i, --image=[FILE]  Image file
      -p                  Print all entrys
      --print=X           Print the target of path X
      -x                  Extract all items
      --extract=X         Extract the target of path X
      -f, --overwrite     [default: skip] overwrite files that already exist
      -T#                 [1-X] Use # threads, -T0: X/2
      --only-cfg          Only extract fs_config and file_contexts
      -o, --outdir=X      Output dir
      -V, --version       Print the version info
    
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

---
+++ build/cmake/erofs-tools/extract.cmake
@@ -0,0 +1,12 @@
+###############################------extract.erofs------###############################
+set(TARGET_extract extract.erofs)
+set(TARGET_SRC_DIR "${PROJECT_ROOT_DIR}/extract")
+file(GLOB extract_srcs "${TARGET_SRC_DIR}/*.cpp")
+add_executable(${TARGET_extract} ${extract_srcs})
+target_include_directories(${TARGET_extract} PRIVATE
+        "${TARGET_SRC_DIR}/include"
+        ${common_headers}
+        ${liblog_headers}
+)
+target_link_libraries(${TARGET_extract} ${common_static_link_lib})
+target_compile_options(${TARGET_extract} PRIVATE ${common_compile_flags})
