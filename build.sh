07125f7e3db03fc2e87b7dd1a94099fcfee30590
sekaiacg <sekaiacg@gmail.com>
2023-03-03 21:03:16 +0800

    CI: Clear unnecessary time information
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- build.sh
+++ build.sh
@@ -74,6 +74,7 @@ build()
 		echo "复制文件中..."
 		[[ ! -d "$TARGET_DIR_PATH" ]] && mkdir -p ${TARGET_DIR_PATH}
 		cp -af $BUILD/*.erofs ${TARGET_DIR_PATH}
+		touch -c -d "2009-01-01 00:00:00" ${TARGET_DIR_PATH}/*
 		echo "编译成功: ${TARGE_DIR_NAME}"
 	else
 		echo "error"
