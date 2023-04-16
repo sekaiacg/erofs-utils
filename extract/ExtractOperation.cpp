1273794860cc3fe6b1216890e391b47d290d6a8b
sekaiacg <sekaiacg@gmail.com>
2023-04-16 14:45:49 +0800

    extract: Fix 'Do not check root path on Windows'
    This commit is: e7714af6ce33e61e92613335f1667f2cf5d313c8
    
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- extract/ExtractOperation.cpp
+++ extract/ExtractOperation.cpp
@@ -53,7 +53,7 @@ namespace skkk {
 			configDir = "./config";
 			outDir = "./" + imgBaseName;
 		} else {
-#if !defined(_WIN32) || !defined(__CYGWIN__)
+#if !(defined(_WIN32) || defined(__CYGWIN__))
 			const char *oDir = outDir.c_str();
 			auto oSize = outDir.size();
 			// check dir is root: "/","//","///",...
