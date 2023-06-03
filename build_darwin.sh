3b904f27c065b8b63b4095fc765239a1dda2812f
sekaiacg <sekaiacg@gmail.com>
2023-06-03 11:52:19 +0800

    cmake: Sync upstream changes
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- build_darwin.sh
+++ build_darwin.sh
@@ -26,7 +26,8 @@ cmake_build()
 			-DCMAKE_CXX_COMPILER_LAUNCHER="ccache" \
 			-DCMAKE_C_COMPILER="clang" \
 			-DCMAKE_CXX_COMPILER="clang++" \
-			-DENABLE_FULL_LTO="ON"
+			-DENABLE_FULL_LTO="ON" \
+			-DMAX_BLOCK_SIZE="4096"
 	fi
 
 	${MAKE_CMD}
