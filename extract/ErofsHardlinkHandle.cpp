1f40e0b26bb705a48798c84e1a580f8ae26ed59b
sekaiacg <sekaiacg@gmail.com>
2023-07-16 18:47:32 +0800

    extract: ErofsHardlinkHandle: Use uint64_t instead of erofs_nid_t
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- extract/ErofsHardlinkHandle.cpp
+++ extract/ErofsHardlinkHandle.cpp
@@ -2,18 +2,18 @@
 
 namespace skkk {
 
-	ErofsHardlinkEntry::ErofsHardlinkEntry(erofs_nid_t _nid, const char *_path) {
+	ErofsHardlinkEntry::ErofsHardlinkEntry(uint64_t _nid, const char *_path) {
 		this->nid = _nid;
 		this->path = _path;
 	}
 
-	int erofsHardlinkInsert(erofs_nid_t nid, const char *path) {
+	int erofsHardlinkInsert(uint64_t nid, const char *path) {
 		if (!path) return -ENOENT;
 		erofsHardlinkMap[nid % NR_HARDLINK_HASHTABLE] = {nid, path};
 		return 0;
 	}
 
-	const char *erofsHardlinkFind(erofs_nid_t nid) {
+	const char *erofsHardlinkFind(uint64_t nid) {
 		auto it = erofsHardlinkMap.find(nid % NR_HARDLINK_HASHTABLE);
 		if (it != erofsHardlinkMap.end()) {
 			if (it->second.nid == nid) {
