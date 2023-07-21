400e24ac8fc5b10879684b819ccac8ea42722347
sekaiacg <sekaiacg@gmail.com>
2023-07-21 11:03:59 +0800

    extract: Generate mkfs.erofs options
    The options are for reference only, please modify according to the actual situation.
    
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- lib/liberofs_uuid.h
+++ lib/liberofs_uuid.h
@@ -2,8 +2,17 @@
 #ifndef __EROFS_LIB_UUID_H
 #define __EROFS_LIB_UUID_H
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 void erofs_uuid_generate(unsigned char *out);
 void erofs_uuid_unparse_lower(const unsigned char *buf, char *out);
 int erofs_uuid_parse(const char *in, unsigned char *uu);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
