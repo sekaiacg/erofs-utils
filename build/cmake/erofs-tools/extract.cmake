6f37671bdccef89b4876e945fe67e913f74011f5
sekaiacg <sekaiacg@gmail.com>
2025-10-24 20:24:57 +0800

    extract: Add build time
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- build/cmake/erofs-tools/extract.cmake
+++ build/cmake/erofs-tools/extract.cmake
@@ -9,3 +9,12 @@ target_include_directories(${TARGET_extract} PRIVATE
 )
 target_link_libraries(${TARGET_extract} ${common_static_link_lib})
 target_compile_options(${TARGET_extract} PRIVATE ${common_compile_flags} "-Wno-unused-result")
+
+set(ENV{TZ} UTF-8)
+execute_process(
+	COMMAND date "+%y%m%d%H%M"
+	OUTPUT_VARIABLE EXTRACT_BUILD_TIME
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+target_compile_definitions(${TARGET_extract} PRIVATE "-DEXTRACT_BUILD_TIME=\"-${EXTRACT_BUILD_TIME}\"")
+MESSAGE(STATUS "[extract] build time is ${EXTRACT_BUILD_TIME}")
