fa9c43e1d71d4519c527eada63a444594cc13381
sekaiacg <sekaiacg@gmail.com>
2026-08-12 13:27:04 +0800

    lib: Update lz4/selinux/xxHash/xz/zlib/zstd
    1.xz: 5.8.3
    2.zlib: 1.3.2.1-motley
    
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- src/lib/libz.cmake
+++ src/lib/libz.cmake
@@ -10,36 +10,36 @@ set(TARGET_CFLAGS
     "-O3"
     "-Wall"
     "-Werror"
+    "-Wno-deprecated-non-prototype"
     "-Wno-unused"
     "-Wno-unused-parameter"
 )
 
-set(cflags_arm
-    "-DINFLATE_CHUNK_SIMD_NEON"
-    "-DADLER32_SIMD_NEON"
-    "-DCRC32_ARMV8_CRC32"
-    "-DDEFLATE_SLIDE_HASH_NEON"
+set(cflags_lp64
+    "-DDEFLATE_CHUNK_WRITE_64LE"
+    "-DINFLATE_CHUNK_READ_64LE"
 )
-set(cflags_arm64 ${cflags_arm} "-DINFLATE_CHUNK_READ_64LE")
+set(cflags_arm
+    "-DADLER32_SIMD_NEON"
+    "-DDEFLATE_SLIDE_HASH_NEON"
+    "-DINFLATE_CHUNK_SIMD_NEON"
+    "-DCRC32_ARMV8_CRC32"
+)
+set(cflags_arm64 ${cflags_arm} ${cflags_lp64})
 set(cflags_riscv64
     "-DRISCV_RVV"
     "-DADLER32_SIMD_RVV"
     "-DDEFLATE_SLIDE_HASH_RVV"
     "-DINFLATE_CHUNK_GENERIC"
-    "-DINFLATE_CHUNK_READ_64LE"
+    ${cflags_lp64}
 )
 set(cflags_x86
-    "-DX86_NOT_WINDOWS"
-    "-DINFLATE_CHUNK_SIMD_SSE2"
     "-DADLER32_SIMD_SSSE3"
     "-DDEFLATE_SLIDE_HASH_SSE2"
+    "-DINFLATE_CHUNK_SIMD_SSE2"
     "-DCRC32_SIMD_SSE42_PCLMUL"
 )
-set(cflags_x86_64
-    ${cflags_x86}
-    "-DDINFLATE_CHUNK_READ_64LE"
-)
-set(cflags_android_x86 "-DADLER32_SIMD_SSSE3")
+set(cflags_x86_64 ${cflags_x86} ${cflags_lp64})
 
 set(libz_srcs
     "adler32.c"
@@ -56,12 +56,23 @@ set(libz_srcs
     "gzwrite.c"
     "infback.c"
     "inffast.c"
-    "inflate.c"
     "inftrees.c"
     "trees.c"
     "uncompr.c"
     "zutil.c"
+    "contrib/optimizations/inffast_chunk.c"
+    "contrib/optimizations/inflate.c"
 )
+
+# Similarly, we don't want optimizations.
+if (USE_LIBZ_STABLE_VERSION OR CMAKE_SYSTEM_PROCESSOR MATCHES "loongarch64|riscv64")
+    list(REMOVE_ITEM libz_srcs
+        "contrib/optimizations/inffast_chunk.c"
+        "contrib/optimizations/inflate.c"
+    )
+    list(APPEND libz_srcs "inflate.c")
+endif ()
+
 list(TRANSFORM libz_srcs PREPEND "${TARGET_SRC_DIR}/")
 
 add_library(${TARGET} STATIC ${libz_srcs})
@@ -70,9 +81,9 @@ set_target_properties(${TARGET} PROPERTIES ARCHIVE_OUTPUT_DIRECTORY ${LIBZ_BINAR
 
 if (CMAKE_SYSTEM_PROCESSOR STREQUAL "arm" OR CMAKE_SYSTEM_PROCESSOR STREQUAL "armv7-a")
     list(APPEND TARGET_CFLAGS ${cflags_arm})
-    if (CMAKE_SYSTEM_NAME STREQUAL "Android")
+    if (CMAKE_SYSTEM_NAME MATCHES "Android|Linux")
         list(APPEND TARGET_CFLAGS "-DARMV8_OS_LINUX")
-    elseif (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    elseif (CMAKE_SYSTEM_NAME MATCHES "Windows|CYGWIN")
         list(APPEND TARGET_CFLAGS "-DARMV8_OS_WINDOWS")
     endif ()
 elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "arm64|aarch64")
@@ -81,26 +92,30 @@ elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "arm64|aarch64")
         list(APPEND TARGET_CFLAGS "-DARMV8_OS_LINUX")
     elseif (CMAKE_SYSTEM_NAME STREQUAL "Darwin")
         list(APPEND TARGET_CFLAGS "-DARMV8_OS_MACOS")
-    elseif (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    elseif (CMAKE_SYSTEM_NAME MATCHES "Windows|CYGWIN")
         list(APPEND TARGET_CFLAGS "-DARMV8_OS_WINDOWS")
     endif ()
 elseif (CMAKE_SYSTEM_PROCESSOR STREQUAL "riscv64")
     list(APPEND TARGET_CFLAGS ${cflags_riscv64})
-elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "i686")
+elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "i686" OR CMAKE_SYSTEM_PROCESSOR STREQUAL "x86")
     list(APPEND TARGET_CFLAGS ${cflags_x86})
-    if (CMAKE_SYSTEM_NAME STREQUAL "Android")
-        list(APPEND TARGET_CFLAGS ${cflags_android_x86})
+    if (CMAKE_SYSTEM_NAME MATCHES "Android|Linux")
+        list(APPEND TARGET_CFLAGS "-DX86_NOT_WINDOWS")
+    elseif (CMAKE_SYSTEM_NAME MATCHES "Windows|CYGWIN")
+        list(APPEND TARGET_CFLAGS "-DX86_WINDOWS")
     endif ()
 elseif (CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
     list(APPEND TARGET_CFLAGS ${cflags_x86_64})
-    if (CMAKE_SYSTEM_NAME STREQUAL "Android")
-        list(APPEND TARGET_CFLAGS ${cflags_android_x86})
+    if (CMAKE_SYSTEM_NAME MATCHES "Android|Linux|Darwin")
+        list(APPEND TARGET_CFLAGS "-DX86_NOT_WINDOWS")
+    elseif (CMAKE_SYSTEM_NAME MATCHES "Windows|CYGWIN")
+        list(APPEND TARGET_CFLAGS "-DX86_WINDOWS")
     endif ()
 endif ()
 
 target_include_directories(${TARGET} PUBLIC ${TARGET_SRC_DIR})
 
-if (CMAKE_SYSTEM_PROCESSOR MATCHES "i686|x86_64")
+if (CMAKE_SYSTEM_PROCESSOR MATCHES "i686|x86_64|x86")
     list(APPEND TARGET_CFLAGS "-msse4.2" "-mpclmul")
 elseif (CMAKE_SYSTEM_PROCESSOR MATCHES "arm64|aarch64")
     list(APPEND TARGET_CFLAGS "-march=armv8-a+crypto")
